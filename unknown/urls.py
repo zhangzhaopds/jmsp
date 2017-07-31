9e894f8aaeaaec89acac57ac6a0ed25ce90c6d44
zhangzhaopds <zhangzhaopds@foxmail.com>
2017-07-31 16:30:58 +0800

    pexel

--- unknown/urls.py
+++ unknown/urls.py
@@ -9,5 +9,10 @@ urlpatterns = [
     url(r'^logout', views.logout, name='logout'),
     url(r'^userInfo', views.userInfo, name='userInfo'),
     url(r'upload', views.upload, name='upload'),
-
+    url(r'homeBgImg', views.homeBgImg, name='homeBgImg'),
+    url(r'searchTags', views.searchTags, name='searchTags'),
+    url(r'popularSearches', views.popularSearches, name='popularSearches'),
+    url(r'photos', views.photos, name='photos'),
+    url(r'discover', views.discover, name='discover'),
+    url(r'leaderboard', views.leaderboard, name='leaderboard'),
 ]
--- unknown/views.py
+++ unknown/views.py
@@ -1,3 +1,5 @@
+from bs4 import BeautifulSoup
+import requests
 from django.http import HttpResponse
 from  pymongo import MongoClient
 from django.core.mail import EmailMessage
@@ -226,7 +228,7 @@ def userInfo(request):
                "msg": 'Account or password is wrong'}
         return HttpResponse(json.dumps(msg), content_type='application/json')
 
-# 照片上传
+# 照片上传 POST: loginName + password + file
 def upload(request):
     loginName = request.POST.get('loginName', None)
     password = request.POST.get('password', None)
@@ -288,4 +290,193 @@ def upload(request):
            },
            "msg": 'Upload successfully'}
     return HttpResponse(json.dumps(msg), content_type='application/json')
+
+# 主页背景照片
+def homeBgImg(request):
+    pexel = 'https://www.pexels.com'
+    req = requests.get(pexel)
+    soup = BeautifulSoup(req.text)
+    bgImg = soup.find_all(name='style')
+    sub = bgImg[0].string.split('(')
+    large = pexel + sub[1].rstrip(');}@media ')
+    small = pexel + sub[len(sub) - 1].rstrip(');}}')
+    json_str = json.dumps({
+        'homeBgImages': {'large': large,
+                         'small': small}
+    })
+    return HttpResponse(json_str, content_type='application/json')
+
+# 搜索标签
+def searchTags(request):
+    pexel = 'https://www.pexels.com'
+    req = requests.get(pexel)
+    soup = BeautifulSoup(req.text)
+    search__tags = soup.find_all(class_='search__tags')
+    results = []
+    for tag in search__tags[0]:
+        if tag.name == 'li':
+            results.append({
+                'searchTagName': tag.a.string,
+                'searchTagHref': tag.a['href']
+            })
+    json_str = json.dumps(results)
+    return HttpResponse(json_str, content_type='application/json')
+
+# 分类搜索: page
+def popularSearches(request):
+    page = request.GET.get('page', '1')
+    pexel = 'https://www.pexels.com/popular-searches/?page=' + page
+    req = requests.get(pexel)
+    soup = BeautifulSoup(req.text)
+    search_medium = soup.find_all(class_='search-medium__link')
+    results = []
+    for tag in search_medium:
+        print(tag['href'])
+        print(tag.h4.string)
+        print(tag.img['src'])
+        results.append({
+            'searchMediumHref': tag['href'],
+            'searchMediumText': tag.h4.string,
+            'searchMediumSrc': tag.img['src']
+        })
+    json_str = json.dumps(results)
+    return HttpResponse(json_str, content_type='application/json')
+
+# 照片： 搜索、主页、流行
+# 搜索板块： href = '/search/job/'
+# Popular Photos： 过去三十天 href = '/popular-photos/'
+# Popular Photos： 所有      href = '/popular-photos/all-time/'
+# 首页100张高质量照片：      href = ''
+def photos(request):
+    href = request.GET.get('href', '')
+    page = request.GET.get('page', '1')
+    pexel = 'https://www.pexels.com' + href
+    post_data = {'page': page}
+    req = requests.get(pexel, data=post_data)
+    soup = BeautifulSoup(req.text)
+    photos = soup.find_all(class_='photo-item')
+    results = []
+    for photo in photos:
+        style = photo.img['style'][15:].rstrip(')')
+        results.append({'href': photo.a['href'],
+                        'alt': photo.img['alt'],
+                        'data-pin-media': photo.img['data-pin-media'],
+                        'src': photo.img['src'],
+                        'height': photo.img['height'],
+                        'width': photo.img['width'],
+                        'style': style})
+    json_str = json.dumps(results)
+    return HttpResponse(json_str, content_type='application/json')
+
+# 发现模块，筛选条件
+def selected(tag):
+    if tag.name == 'section':
+        if tag['class'] == ['photo-row'] or tag['class'] == ['photo-row', 'photo-row--user']:
+            return True
+
+
+# 发现: page
+def discover(request):
+    page = request.GET.get('page', '1')
+    pexel = 'https://www.pexels.com/discover/?page=' + page
+    req = requests.get(pexel)
+    soup = BeautifulSoup(req.text)
+    sections = soup.find_all(selected)
+    print(len(sections))
+    results = []
+    for section in sections:
+        section_href = ''
+        section_title = ''
+        imgs = []
+        for content in section.contents:
+            if content.name == 'header':
+                section_href = content.h2.a['href']
+                for title in content.h2.a.stripped_strings:
+                    section_title = title
+            if content.name == 'div':
+                for article in content.div.contents:
+                    if article.name == 'article':
+                        imgs.append({
+                            'href': article.a['href'],
+                            'alt': article.a.img['alt'],
+                            'width': article.a.img['width'],
+                            'height': article.a.img['height'],
+                            'data-pin-media': article.a.img['data-pin-media'],
+                            'src': article.a.img['src'],
+                            'style': article.a.img['style'][15:].rstrip(')')
+                        })
+        if len(imgs) > 0:
+            results.append({
+                'section_href': section_href,
+                'section_title': section_title,
+                'imgs': imgs
+            })
+    json_str = json.dumps(results)
+    return HttpResponse(json_str, content_type='application/json')
+
+# Leaderboard: href = ''    href = 'all-time/'
+def leaderboard(request):
+    href = request.GET.get('href', '')
+    page = '?page=' + request.GET.get('page', '1')
+    pexel = 'https://www.pexels.com/leaderboard/' + href + page
+    req = requests.get(pexel)
+    soup = BeautifulSoup(req.text)
+    articles = soup.find_all(class_='leaderboard')
+    results = []
+    for article in articles:
+        profile_number = 0  # 排行
+        profile_downTimes = 0  # 下载量
+        profile_href = ''
+        profile_name = ''
+        profile_site = ''
+        profile_site_name = ''
+        profile_avatar = ''
+        leaderboard__photos = []
+        for cont in article.contents:
+            if cont.name == 'div':
+                if cont['class'] == ['mini-profile', 'leaderboard__profile']:
+                    for profile in cont.contents:
+                        if profile.name == 'div':
+                            if profile['class'] == ['leaderboard__place']:
+                                for num in [profile.contents[1]][0].stripped_strings:
+                                    profile_number = num
+                                for num in [profile.contents[3]][0].stripped_strings:
+                                    profile_downTimes = num
+                            if profile['class'] == ['leaderboard__profile-information']:
+                                for link in profile.contents:
+                                    if link.name == 'h3':
+                                        profile_href = profile.h3.a['href']
+                                        profile_name = profile.h3.a.string
+                                    if link.name == 'a':
+                                        profile_site = link['href']
+                                        profile_site_name = link.string
+                        if profile.name == 'a':
+                            profile_avatar = profile.img['src']
+
+                if cont['class'] == ['leaderboard__photos']:
+                    for photo in cont.contents:
+                        if photo.name == 'a':
+                            if photo['class'] == ['leaderboard__photo']:
+                                leaderboard__photos.append({
+                                    'href': photo['href'],
+                                    'src': photo.img['src'],
+                                    'height': photo.img['height'],
+                                    'width': photo.img['width']
+                                })
+        leaderboard__profile = {
+            'number': profile_number,
+            'downTimes': profile_downTimes,
+            'href': profile_href,
+            'name': profile_name,
+            'site': profile_site,
+            'siteName': profile_site_name,
+            'avatar': profile_avatar
+        }
+        results.append({
+            'leaderboard_profile': leaderboard__profile,
+            'leaderboard__photos': leaderboard__photos
+        })
+    json_str = json.dumps(results)
+    return HttpResponse(json_str, content_type='application/json')
+
 
